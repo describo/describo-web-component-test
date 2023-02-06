586b6a03c307f14c2a38fa098417c185f179365e
Balazs E. Pataki <pataki@gmail.com>
2023-02-06 11:31:23 +0100

    Fix inclusion of css in web components
    There might be a more straightforward way using but I couldn't get it working:
    https://vuejs.org/guide/extras/web-components.html#building-custom-elements-with-vue

--- src/crate-builder/index.wc.js
+++ src/crate-builder/index.wc.js
@@ -8,6 +8,16 @@ import "@fortawesome/fontawesome-free/js/all";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoReplaceSvg = "nest";
 
+// The trick is to import from the vue build, because it is built before this, so style.css is already available.
+// If we tried to import from dist/web-component/style.css it was not available since we are in the building process
+// and so style.css wouldbe only available at the end of it.
+import css from "../../dist/vue/style.css"
+
+// Explicitly inline styles in the component
+DescriboCrateBuilder.styles = [css]
+
 // Web Component build entrypoint
 const element = defineCustomElement(DescriboCrateBuilder);
+
+
 export default customElements.define("describo-crate-builder", element);
